5f1b3f1aca90a0103062789433cf133edebefcd6
Ines Radic <inesradic12314@gmail.com>
2021-04-14 13:20:41 -0700

    add mongoose connection

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const path = require("path");
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+// morgan
+app.use(logger('dev'));
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// Check if database is the problem
+const db = mongoose.connection
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to Database'));
 
 // Define API routes here
 
