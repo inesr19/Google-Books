95dbb651b90dca25eb541eda906548ef2fff6565
Ines Radic <inesradic12314@gmail.com>
2021-04-15 20:43:56 -0700

    create nav tab

---
+++ client/src/components/pages/Nav/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Nav, Navbar} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function NavbarTab() {
+
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand as={Link} to="/">Google Books</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="justify-content-end">
+                    <Nav.Link as={Link} to="/search">Search</Nav.Link>
+                    <Nav.Link as={Link} to="/save">Saved Books</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        
+      </div>
+    );
+  }
+  
+  export default NavbarTab;
